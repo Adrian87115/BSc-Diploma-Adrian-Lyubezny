95725fd735ac01dbf9dfb219e2f6c72ccc8ecae0
Adrian Lyubezny <adrianlyubeznyy04@gmail.com>
2026-09-18 16:31:28 +0200

    chore: updated to do

--- gastroent/main.py
+++ gastroent/main.py
@@ -7,8 +7,13 @@ from utils_m.tests import test_classification, test_segmentation
 # GENERIC CLASSES FROM KVASIR MAY CONFUSE THE SPECIFIC CLASSES IN HYPERKVASIR. 
 # THIS IS NOT AN ISSUE IF DATASETS ARE NOT MERGED OR CROSS-TESTED.
 
+# WHEN USING AUGMENTATIONS IT IS IMPORTANT THAT OPERATIONS DO NOT COLLIDE WITH ORDER AND OPERATIONS OF PREP.
+# CENTERCROP AND RANDOMCROP MAY COLLIDE.
+
+# TRAINING ON CPU AND GPU MAY DIFFER, DUE TO DIFFERENCES IN KRONIA AND ALBUMENTATIONS.
+# IT IS IMPORTANT TO TRACK WHERE MODEL IS TRAINED, AND WHAT AUGMENTATIONS ARE USED.
+
 # TO DO:
-# augmentations
 # DATALOADER IS REPSONSOBLE FOR THE OVERSAMPLING/UNDERSAMPLING  WeightedRandomSamplerXXXDistributedSampler
 
 def main(seed):
@@ -16,7 +21,7 @@ def main(seed):
     # run_segmentation_eda()
 
     classification_experiment(seed, 'Kvasir1')
-    # segmentation_experiment(seed, 'KvasirSeg1')
+    segmentation_experiment(seed, 'KvasirSeg1')
 
     # test_classification(ResNetModel(3, 24, '18'), 'Kvasir1', 2, 20, 'HyperKvasir/lower-gi-tract/pathological-findings/polyps/2dadc75e-8fca-4411-88a0-65a3f1cc92be.jpg', labels = 'polyps')
-    # test_segmentation(DeepLabV3PlusModel(3, 'resnet18'), 'KvasirSeg1', 3, 20, 'CVC-ClinicDB', ['3.png', '26.png'])
+    # test_segmentation(DeepLabV3PlusModel(3, 'resnet18'), 'KvasirSeg1', 2, 20, 'CVC-ClinicDB', ['3.png', '26.png'])
